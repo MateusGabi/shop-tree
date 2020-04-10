a25a109268aff14f93c86a69c02e83a48b430e12
Mateus Gabi Moreira <mateusgabimoreira@gmail.com>
2020-04-10 16:25:25 -0300

    adicionado SafeAreaView

--- Screens/HomeScreen.js
+++ Screens/HomeScreen.js
@@ -1,9 +1,9 @@
 import { Navbar, UserSession } from "../Components/Navbar";
-import { SafeAreaView, ScrollView, StyleSheet, Text } from "react-native";
+import { SafeAreaView, ScrollView } from "react-native";
 
 import Ad from "../Components/Ad";
 import Body from "../Components/Body";
-import COLORS from '../constants/colors.json'
+import COLORS from "../constants/colors.json";
 import Menu from "../Components/Menu";
 import ProductList from "../Components/ProductList";
 import React from "react";
@@ -12,13 +12,15 @@ import UsersList from "../Components/UsersList";
 
 export default function HomeScreen() {
   return (
-    <SafeAreaView style={{ backgroundColor: COLORS.background.main}}>
+    <SafeAreaView style={{ backgroundColor: COLORS.background.main }}>
       <Navbar />
       <UserSession />
       <Menu />
       <Search />
-      <Ad />
-      <ProductList />
+      <ScrollView>
+        <Ad />
+        <ProductList />
+      </ScrollView>
     </SafeAreaView>
   );
 }
